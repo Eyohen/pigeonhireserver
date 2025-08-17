97a5fd77ea47a9af4a7fa3ccf629fb4be6ba488a
Eyohen <henry.eyo2@gmail.com>
2025-08-17 17:55:21 +0100

    userfavorite redoings

--- models/userFavorite.js
+++ models/userFavorite.js
@@ -79,7 +79,14 @@
 
 
 
-// models/userFavorite.js - Fixed version with proper nullable fields
+
+
+
+
+
+// models/userFavorite.js - CORRECTED VERSION
+// Replace your existing userFavorite.js file with this content
+
 'use strict';
 
 const { Model, UUIDV4 } = require('sequelize');
@@ -125,7 +132,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     communityId: {
       type: DataTypes.UUID,
-      allowNull: true, // IMPORTANT: Must be nullable
+      allowNull: true, // CRITICAL: Must be nullable for connector favorites
       references: {
         model: 'Communities',
         key: 'id'
@@ -134,7 +141,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     connectorId: {
       type: DataTypes.UUID,
-      allowNull: true, // IMPORTANT: Must be nullable
+      allowNull: true, // CRITICAL: Must be nullable for community favorites
       references: {
         model: 'Connectors',
         key: 'id'
@@ -144,7 +151,9 @@ module.exports = (sequelize, DataTypes) => {
     favoriteType: {
       type: DataTypes.ENUM('community', 'connector'),
       allowNull: false,
-      defaultValue: 'community'
+      validate: {
+        isIn: [['community', 'connector']]
+      }
     },
     createdAt: {
       type: DataTypes.DATE,
@@ -160,6 +169,8 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'UserFavorite',
     tableName: 'UserFavorites',
+    
+    // Custom validation to ensure either communityId OR connectorId is provided
     validate: {
       eitherCommunityOrConnector() {
         if (!this.communityId && !this.connectorId) {
@@ -167,6 +178,62 @@ module.exports = (sequelize, DataTypes) => {
         }
         if (this.communityId && this.connectorId) {
           throw new Error('Cannot favorite both community and connector in the same record');
+        }
+        
+        // Ensure favoriteType matches the provided ID
+        if (this.communityId && this.favoriteType !== 'community') {
+          throw new Error('favoriteType must be "community" when communityId is provided');
+        }
+        if (this.connectorId && this.favoriteType !== 'connector') {
+          throw new Error('favoriteType must be "connector" when connectorId is provided');
+        }
+      }
+    },
+
+    // Custom indexes (Sequelize will create these)
+    indexes: [
+      // Unique constraint for community favorites
+      {
+        unique: true,
+        fields: ['userId', 'communityId'],
+        where: {
+          communityId: {
+            [sequelize.Sequelize.Op.ne]: null
+          }
+        },
+        name: 'user_community_favorite_unique'
+      },
+      // Unique constraint for connector favorites
+      {
+        unique: true,
+        fields: ['userId', 'connectorId'],
+        where: {
+          connectorId: {
+            [sequelize.Sequelize.Op.ne]: null
+          }
+        },
+        name: 'user_connector_favorite_unique'
+      },
+      // Index for faster lookups by favoriteType
+      {
+        fields: ['favoriteType']
+      },
+      // Index for faster lookups by userId and favoriteType
+      {
+        fields: ['userId', 'favoriteType']
+      }
+    ],
+
+    // Hooks to automatically set favoriteType based on which ID is provided
+    hooks: {
+      beforeValidate: (favorite, options) => {
+        // Auto-set favoriteType if not provided
+        if (!favorite.favoriteType) {
+          if (favorite.communityId) {
+            favorite.favoriteType = 'community';
+          } else if (favorite.connectorId) {
+            favorite.favoriteType = 'connector';
+          }
         }
       }
     }
